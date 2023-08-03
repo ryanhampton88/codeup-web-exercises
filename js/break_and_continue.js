e5c64a21acb9d6fdb8a7d967baf78be5cfcf0c4e
Ryan Hampton <hamptonryan88@gmail.com>
2023-08-03 16:59:26 -0500

    started loop exercises. stopped at break and continue

---
+++ js/break_and_continue.js
@@ -0,0 +1,4 @@
+let userInput = 0;
+do {
+    userInput = prompt("Enter an odd number between 1 and 50:")
+} while (userInput === userInput % 2)
---
+++ js/for_loops.js
@@ -0,0 +1,30 @@
+// function showMultiplicationTable(num) {
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(num +" * " + i  + " = " + num * i);
+//     }
+// }
+// showMultiplicationTable(7);
+
+// function isEvenOrOdd() {
+//     for (let i = 1; i <= 10; i++) {
+//         let random = Math.floor(Math.random() * (200 - 20 + 1) + 20);
+//         if (random % 2 === 0) {
+//             console.log(random + " is even");
+//         }
+//         if (random % 2 !== 0) {
+//             console.log(random + " is odd");
+//         }
+//     }
+// }
+//
+// isEvenOrOdd();
+
+// for (i = 1; i < 10; i++) {
+//     for (j = 1; j <= i; j++) {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 100; i >= 5; i-=5) {
+//     console.log(i);
+// }
