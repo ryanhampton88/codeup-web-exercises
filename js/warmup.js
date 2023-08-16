e23198ac8b37a791d9cc4bab5601f89204bd7bde
Ryan Hampton <hamptonryan88@gmail.com>
2023-08-16 10:14:46 -0500

    finished warmup and css selectors exercise -rh

--- js/warmup.js
+++ js/warmup.js
@@ -1,11 +1,30 @@
-function logUser(input){
-    if (typeof input !== 'string'){
+let userObject = {
+    userName: "ken2cool",
+    userEmail: "ken2cool@yahoo.com"
+};
+
+
+console.log(userObject.userName);
+console.log(userObject.userEmail);
+
+function logUser(){
+    if (typeof userObject.userName !== 'string') {
         return false;
     }
-    return `${input} has logged in for the day.`;
-
+    return `${userObject.userName} with email ${userObject.userEmail} has logged in for the day.`;
 }
 
-console.log(logUser("ryanhampton"));
+console.log(logUser(userObject));
 
-console.log(logUser(2334));
+
+userObject.userRoles = [ "admin", "moderator", "buyer", "seller", "reviewer"];
+
+console.log(userObject.userRoles);
+
+userObject.userProfile = {
+    userProfileUrl: "myProfileLink",
+    userAge: 31,
+    userLocation: "San Antonio"
+}
+
+console.log(userObject.userProfile);
