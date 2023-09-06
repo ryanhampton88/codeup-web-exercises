455d7ffda3e6a0f9c1e5510dcf97b198e23b3810
Ryan Hampton <hamptonryan88@gmail.com>
2023-09-06 13:52:52 -0500

    finished jQuery exercises

--- js/jQuery-js.js
+++ js/jQuery-js.js
@@ -1,3 +1,19 @@
-$(function(){
-    alert("Document has loaded!")
-})
+// *** Id selectors exercise ***
+
+// $(function(){
+//     // alert("Document has loaded!")
+// })
+//
+// alert($("#ice-cream-list").html());
+//
+// alert($("#para").html());
+
+// *** Class selectors exercise ***
+
+// $(".codeup").css("border", "1px solid red");
+
+//*** Element selectors exercise ***
+
+$("li, h1, p").css("background-color", "yellow");
+
+alert($("h1").html());
