c678eeacc78cb60ea6c1801624ce8c6434e43b90
Ryan Hampton <hamptonryan88@gmail.com>
2023-09-06 15:43:32 -0500

    finished mouse event exercises

--- js/jQuery-js.js
+++ js/jQuery-js.js
@@ -1,5 +1,4 @@
 // *** Id selectors exercise ***
-
 // $(function(){
 //     // alert("Document has loaded!")
 // })
@@ -8,12 +7,28 @@
 //
 // alert($("#para").html());
 
-// *** Class selectors exercise ***
 
+// *** Class selectors exercise ***
 // $(".codeup").css("border", "1px solid red");
 
+
 //*** Element selectors exercise ***
+// $("li, h1, p").css("background-color", "yellow");
+//
+// alert($("h1").html());
 
-$("li, h1, p").css("background-color", "yellow");
+// *** Mouse Events Exercise ***
+$("h1").on("click", function(){
+    $("h1").css("background-color", "yellow");
+})
 
-alert($("h1").html());
+$("p").on("dblclick", function(){
+    $("p").css("font-size", "18px");
+})
+
+$("li").hover(function(){
+    $("li").css("color", "red");
+},
+    function(){
+    $("li").css("color", "black");
+    })
